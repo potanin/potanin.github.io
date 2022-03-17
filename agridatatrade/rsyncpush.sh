83b242f74b402ca25e934a2a3ee56af38e9861d1
Alex Potanin <alex@ecs.vuw.ac.nz>
2022-03-17 17:39:29 +1300

    Porting

--- agridatatrade/rsyncpush.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/tcsh
-rsync -zarvh --delete -n . alex@bats:public_html/agridatatrade/
-echo
-echo If you want the above to happen then run this command. YOU WERE WARNED.
-echo rsync -zarvh --delete . alex@bats:public_html/agridatatrade/
