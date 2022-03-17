bd79358062e2d94af533a1a9cd09a069740e3e55
Alex Potanin <alex@ecs.vuw.ac.nz>
2022-03-17 16:57:01 +1300

    Imported ECS old site.

---
+++ agridatatrade/rsyncpull.sh
@@ -0,0 +1,5 @@
+#!/bin/tcsh
+rsync -zarvh --delete -n alex@bats:public_html/agridatatrade/ .
+echo
+echo If you want the above to happen then run this command. YOU WERE WARNED.
+echo rsync -zarvh --delete alex@bats:public_html/agridatatrade/ .
